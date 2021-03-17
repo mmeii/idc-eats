bc08a71494fff7a0f70b30145fed26f509211125
Kole Gasior <jkole822@gmail.com>
2021-03-16 21:06:29 -0400

    implement passport and add config folder for keys

---
+++ services/passport/google.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const db = require("../../models");
+const keys = require("../../config/keys");
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback",
+			proxy: true,
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			const id = profile.id;
+			const username = profile.displayName;
+
+			const user = await db.User.findOrCreate({
+				where: { id },
+				defaults: {
+					username,
+				},
+			});
+
+			done(null, user);
+		}
+	)
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user[0].dataValues.id);
+});
+
+passport.deserializeUser((id, done) => {
+	db.User.findByPk(id).then(user => done(null, user));
+});
