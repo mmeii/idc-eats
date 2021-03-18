62afca0f9f1fc1d4e84cbf707d55a2362eed234b
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-03-18 13:29:40 -0400

    Installed react, framer motion packages, created file for pages, and added logo

---
+++ client/src/pages/login/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Login () {
+    return(
+        <div>
+        <img src="" alt="idc-eats logo" />
+        <h1>Login with Google</h1>
+        <i class="fab fa-google"></i>
+        </div>
+    )
+}
