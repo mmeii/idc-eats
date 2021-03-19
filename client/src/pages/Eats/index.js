00d44c58155be526def3fd0a269aebcda50a6a14
Mengmei Tu <redmmeii@gmail.com>
2021-03-18 20:38:14 -0500

    add in img placeholder and 2 buttons

--- client/src/pages/Eats/index.js
+++ client/src/pages/Eats/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import PriceCheckbox from '../../components/PriceCheckbox';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+}));
 
-const Eats = () => {
+export default function Eats() {
 
     return (
-        <div>
-            <PriceCheckbox /> $
-            <PriceCheckbox /> $$
-            <PriceCheckbox /> $$$
-            <PriceCheckbox /> $$$$
-        </div>
+        <Container>
+            <div className="eats">
+                <div className="price">
+                    <PriceCheckbox /> $
+                <PriceCheckbox /> $$
+                <PriceCheckbox /> $$$
+                <PriceCheckbox /> $$$$
+            </div>
+
+                <Button variant="contained" color="primary">
+                    Rando
+                </Button>
+
+                <div className="restaurantImage">
+                    <img src="https://via.placeholder.com/300" alt="restaurant" />
+                </div>
+
+                <Button variant="contained" color="primary">
+                    More Info
+            </Button>
+            </div>
+        </Container>
     )
 }
 
-export default Eats;
+
