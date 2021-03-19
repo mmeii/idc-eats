8ccfbd1945f079510d4dacc758c81753a87890c1
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-03-18 20:42:34 -0400

    Added text fields and button to login page. added route for login page on APP

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -5,6 +5,13 @@ function Login () {
     return(
         <div>
         <img src="" alt="idc-eats logo" />
+        <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="email" label="email" />
+        <TextField id="password" label="password" />
+        <Button variant="contained" color="secondary">
+        Secondary
+        </Button>
+        </form>
         <h1>Login with Google</h1>
         <i class="fab fa-google"></i>
         </div>
