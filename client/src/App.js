cf5e8e35cb57d05e51a78220667fc9cd268754a6
Mengmei Tu <redmmeii@gmail.com>
2021-03-18 20:06:30 -0500

    add Eats page for random food

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import Nav from './components/Nav';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Eats from './pages/Eats';
+// import Login from './pages/Login';
+
 
 import "./App.css";
 
@@ -10,9 +13,9 @@ function App() {
       <Nav />
       <Switch>
         {/* ADD in after created component={Login} */}
-        <Route exact path="/login" />
+        <Route exact path="/" />
         {/*  ADD in after created component={Eats} */}
-        <Route exact path="/eats" />
+        <Route exact path="/eats" component={Eats} />
         {/*  ADD in after created component={Preferences} */}
         <Route exact path="/preferences" />
         {/*  ADD in after created component={Logout} */}
---
+++ client/src/pages/Eats/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+
+export default function Checkboxes() {
+    const [checked, setChecked] = React.useState(true);
+
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+    };
+
+    return (
+        <div>
+
+            <Checkbox
+                color="primary"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+
+            <Checkbox
+                color="primary"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+
+            <Checkbox
+                color="primary"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+
+            <Checkbox
+                color="primary"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+
+        </div>
+    );
+}
