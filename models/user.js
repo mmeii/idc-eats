75165d5a340801626a08fa4d58a31632f10c8086
thorgriffs <22.kelliking@gmail.com>
2021-03-18 21:13:41 -0400

    Added User model

---
+++ models/user.js
@@ -0,0 +1,20 @@
+//create User model
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(User, {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+    });
+  
+    return User;
+  };
