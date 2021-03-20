ec6a94d5584c491751717dec6ec12c30c2df2745
Mengmei Tu <redmmeii@gmail.com>
2021-03-20 16:33:54 -0500

    add container component from material ui

---
+++ client/src/components/ContainerWrapper/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+export default function ContainerWrapper(props) {
+    return (
+
+        <Container
+            maxWidth="sm"
+        // alignItems="center"
+        // justifyContent="center"
+        // display="flex"
+        // style={{ minHeight: '100vh' }}
+        >
+            {props.children}
+        </Container>
+
+    );
+}
