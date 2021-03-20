f7ed72c43c5638cd4786ffffe56547c86d110537
Mengmei Tu <redmmeii@gmail.com>
2021-03-20 15:58:24 -0500

    fix merge conflict from main

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -42,28 +42,6 @@ function Login() {
 			</a>
 		</div>
 	);
-=======
-    const handleClick = (event) => {
-
-    };
-    return (
-
-        <div>
-            <motion.div
-                animate={{ scale: 2 }}
-                transition={{ duration: 0.5 }}
-            />
-            <img src={`${process.env.PUBLIC_URL}/assets/logo.jpg`} alt="idc-eats logo" />
-            <form>
-                <input id="email" label="email" />
-                <input id="password" label="password" />
-                <button variant="contained" color="secondary" onClick={handleClick}>Login</button>
-            </form>
-            <h1>Login with Google</h1>
-            <i class="fab fa-google" onClick={handleClick}></i>
-        </div>
-    )
-
 }
 
 export default Login;
