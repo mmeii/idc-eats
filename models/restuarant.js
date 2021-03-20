c92d355398b702405b4fe2b8097dc07daa0cf4ca
thorgriffs <22.kelliking@gmail.com>
2021-03-20 14:16:57 -0400

    added restaurant model and modified preference model

---
+++ models/restuarant.js
@@ -0,0 +1,25 @@
+//create Restaurant model
+module.exports = (sequelize, DataTypes) => {
+    const Restaurant = sequelize.define('Restaurant', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      yelp_id: {
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+    });
+
+    Restaurant.associate = (models) => {
+      Restaurant.hasMany(models.Selection, {
+        onDelete: 'cascade',
+        });
+    };
+
+    return Restaurant;
+  };
