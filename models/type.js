6e185c29935c0214fb819c78ae417419db58f57a
thorgriffs <22.kelliking@gmail.com>
2021-03-20 13:35:29 -0400

    fixed type model

--- models/type.js
+++ models/type.js
@@ -1,6 +1,5 @@
 //create Type model
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD
     const Type = sequelize.define('Type', {
       id: {
         type: DataTypes.INTEGER,
@@ -21,24 +20,3 @@ module.exports = (sequelize, DataTypes) => {
   
     return Type;
   };
-=======
-	const Type = sequelize.define("Type", {
-		id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			autoIncrement: true,
-			primaryKey: true,
-		},
-		type: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-	});
-
-	Type.associate = models => {
-		Type.hasMany(models.Category);
-	};
-
-	return Type;
-};
->>>>>>> 25595fec818a9720911ec21efe274bb691f5db12
