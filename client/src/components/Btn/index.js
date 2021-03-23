4e4a76ee2296d9177449f20e9b3aaa168818a8e9
Mengmei Tu <redmmeii@gmail.com>
2021-03-22 21:01:42 -0500

    update styling on button color

--- client/src/components/Btn/index.js
+++ client/src/components/Btn/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            // light: will be calculated from palette.primary.main,
+            main: '#226126',
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+        },
+        // Used by `getContrastText()` to maximize the contrast between
+        // the background and the text.
+        contrastThreshold: 3,
+        // Used by the functions below to shift a color's luminance by approximately
+        // two indexes within its tonal palette.
+        // E.g., shift from Red 500 to Red 300 or Red 700.
+        tonalOffset: 0.2,
+    },
+});
 
 function Btn(props) {
+
     return (
-        <Button
-            variant="contained"
-            color="primary"
-            onClick={props.onClick}
-        >
-            {props.label}
-        </Button>
+        <ThemeProvider theme={theme}>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={props.onClick}
+            >
+                {props.label}
+            </Button>
+        </ThemeProvider>
     );
 }
 
