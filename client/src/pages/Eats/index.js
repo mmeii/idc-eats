be95c51d319bfda705210a3916ef2e98e66cba52
Kole Gasior <jkole822@gmail.com>
2021-03-26 17:55:30 -0400

    replace checkboxes with PriceForm

--- client/src/pages/Eats/index.js
+++ client/src/pages/Eats/index.js
@@ -1,130 +1,138 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import PriceCheckbox from "../../components/PriceCheckbox";
 import { makeStyles } from "@material-ui/core/styles";
 import Btn from "../../components/Btn";
 import ContainerWrapper from "../../components/ContainerWrapper";
 import "./style.css";
 import RandoAnim from "../../components/RandoAnim";
+import PriceForm from "../../components/PriceForm";
 import axios from "axios";
+import _ from "lodash";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        "& > *": {
-            margin: theme.spacing(1),
-        },
-    },
+	root: {
+		"& > *": {
+			margin: theme.spacing(1),
+		},
+	},
 }));
 
 export default function Eats() {
-    const classes = useStyles();
-    // const [showDetails, setShowDetails] = useState(false);
-    const [coords, setCoords] = useState();
-    const [restaurant, setRestaurant] = useState({});
+	const classes = useStyles();
+	const [coords, setCoords] = useState();
+	const [restaurant, setRestaurant] = useState({});
+	const [priceOptions, setPriceOptions] = useState([]);
 
-    //yelp get route basing on lat and long
-    useEffect(() => {
-        fetchCoords();
-    }, []);
+	//yelp get route basing on lat and long
+	useEffect(() => {
+		fetchCoords();
+	}, []);
 
-    const fetchCoords = () => {
-        navigator.geolocation.getCurrentPosition(res => {
-            setCoords({
-                latitude: res.coords.latitude,
-                longitude: res.coords.longitude,
-            });
-        });
-    };
+	useEffect(() => {
+		fetchRestaurant();
+	}, [priceOptions]);
 
-    const onClick = () => {
-        // setShowDetails(true);
+	const fetchCoords = () => {
+		navigator.geolocation.getCurrentPosition(res => {
+			setCoords({
+				latitude: res.coords.latitude,
+				longitude: res.coords.longitude,
+			});
+		});
+	};
 
-        if (coords) {
-            axios
-                .get(`/api/restaurants/${coords.latitude}/${coords.longitude}`)
-                // .then(res => res.json())
-                .then(result => {
-                    // console.log(result);
-                    setRestaurant(result.data);
-                })
-                .catch(err => console.log(err));
-        } else {
-            console.log("cannot get coords");
-        }
-    };
+	const fetchRestaurant = () => {
+		if (coords) {
+			axios
+				.get(
+					`/api/restaurants/${coords.latitude}/${coords.longitude}?price=${priceOptions}`
+				)
+				// .then(res => res.json())
+				.then(result => {
+					// console.log(result);
+					setRestaurant(result.data);
+				})
+				.catch(err => console.log(err));
+		} else {
+			console.log("cannot get coords");
+		}
+	};
 
-    console.log(restaurant);
-    // open: is_closed
-    // restaurant name: name
-    // image: image_url
-    // rating: rating
-    // location: location.display_address
-    // phone: phone
+	const handleSubmit = formData => {
+		const selectedPrices = _.chain(formData)
+			.map(option => option)
+			.filter(option => option)
+			.flatten()
+			.value();
 
-    {/* onclick to go to google maps with restaurant address */ }
-    const goToRestaurant = () => {
-        window.open(`http://maps.google.com/?q=${restaurant.location.display_address}`, "_blank")
-    };
+		setPriceOptions(selectedPrices);
+	};
 
-    const RestaurantDetails = () => (
-        <div className="oneRestaurant">
-            <div>
-                <img src={restaurant.image_url} alt="restaurant" />
-            </div>
+	console.log(restaurant);
+	// open: is_closed
+	// restaurant name: name
+	// image: image_url
+	// rating: rating
+	// location: location.display_address
+	// phone: phone
 
-            <div className="restDetails">
-                <h3>We found a {restaurant.categories[0].title} restaurant for you!</h3>
-                <p>Restaurant Name: {restaurant.name}</p>
-                <p>Rating: {restaurant.rating}</p>
-                <p>Address: {restaurant.location.display_address[0]}, {restaurant.location.display_address[1]}</p>
-                <p>Phone: {restaurant.display_phone}</p>
-                <h3>Sounds good, right?</h3>
-            </div>
+	{
+		/* onclick to go to google maps with restaurant address */
+	}
+	const goToRestaurant = () => {
+		window.open(
+			`http://maps.google.com/?q=${restaurant.location.display_address}`,
+			"_blank"
+		);
+	};
 
-            <div className={classes.root}>
-                <Btn
-                    variant="contained"
-                    color="primary"
-                    onClick={onClick}
-                    label="Nope!"
-                />
-
-                <Btn label="Yes, Take Me There!"
-                    onClick={goToRestaurant} />
-
-                {/* <Link to="details">
-                    <Btn label="More Info" />
-                </Link> */}
-            </div>
-        </div>
-    );
-
-    const Rando = () => (
-        <div className="eats">
-            <div className="imgDiv">
-                <RandoAnim />
-            </div>
-
-            <div className="price">
-                <PriceCheckbox /> $
-				<PriceCheckbox /> $$
-				<PriceCheckbox /> $$$
-				<PriceCheckbox /> $$$$
+	const RestaurantDetails = () => (
+		<div className="oneRestaurant">
+			<div>
+				<img src={restaurant.image_url} alt="restaurant" />
 			</div>
 
-            {/* rando button to show restaurant */}
-            <Btn
-                variant="contained"
-                color="primary"
-                onClick={onClick}
-                label="Rando"
-            />
-        </div>
-    );
+			<div className="restDetails">
+				<h3>We found a {restaurant.categories[0].title} restaurant for you!</h3>
+				<p>Restaurant Name: {restaurant.name}</p>
+				<p>Rating: {restaurant.rating}</p>
+				<p>
+					Address: {restaurant.location.display_address[0]},{" "}
+					{restaurant.location.display_address[1]}
+				</p>
+				<p>Phone: {restaurant.display_phone}</p>
+				<h3>Sounds good, right?</h3>
+			</div>
 
-    return (
-        <ContainerWrapper>
-            {Object.keys(restaurant).length ? <RestaurantDetails /> : <Rando />}
-        </ContainerWrapper>
-    );
+			<div className={classes.root}>
+				<Btn
+					variant="contained"
+					color="primary"
+					onClick={fetchRestaurant}
+					label="Nope!"
+				/>
+
+				<Btn label="Yes, Take Me There!" onClick={goToRestaurant} />
+
+				{/* <Link to="details">
+                    <Btn label="More Info" />
+                </Link> */}
+			</div>
+		</div>
+	);
+
+	const Rando = () => (
+		<div className="eats">
+			<div className="imgDiv">
+				<RandoAnim />
+			</div>
+
+			<PriceForm handleSubmit={handleSubmit} />
+		</div>
+	);
+
+	return (
+		<ContainerWrapper>
+			{Object.keys(restaurant).length ? <RestaurantDetails /> : <Rando />}
+		</ContainerWrapper>
+	);
 }
