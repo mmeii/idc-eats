992ef00bf9f8e02ed3a881828010c6b1ec24a8da
Kole Gasior <jkole822@gmail.com>
2021-03-26 16:00:37 -0400

    replace loop

--- client/src/components/RandoAnim/index.js
+++ client/src/components/RandoAnim/index.js
@@ -1,24 +1,16 @@
-import  React, { useEffect } from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import "./style.css";
-import  BackgroundSource from "./BackgroundSource";
+import BackgroundSource from "./BackgroundSource";
 
+function RandoAnim() {
+	return (
+		<motion.div
+			className="rando"
+			animate={{ x: [0, 100, 0, -100, 0], rotate: 360 }}
+			transition={{ duration: 3, repeat: Infinity, ease: "linear" }}
+		/>
+	);
+}
 
-    
-
-
-function RandoAnim () {
-
-
-
-    return (
-        <motion.div
-        className = "rando"
-        animate=  {{x: [ 0, 100, 0, -100, 0], rotate: 360}}
-        transition= {{ duration: 3, loop: Infinity, ease: "linear"}}
-        />
-    )
-  }
-
-  export default RandoAnim;
-  
+export default RandoAnim;
