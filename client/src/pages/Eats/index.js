0e0fed65e24d1463a82e623fe62f518b3e58b3bf
Kole Gasior <jkole822@gmail.com>
2021-03-26 16:03:23 -0400

    add checkboxes to handle priceOption state

--- client/src/pages/Eats/index.js
+++ client/src/pages/Eats/index.js
@@ -4,6 +4,7 @@ import PriceCheckbox from "../../components/PriceCheckbox";
 import { makeStyles } from "@material-ui/core/styles";
 import Btn from "../../components/Btn";
 import ContainerWrapper from "../../components/ContainerWrapper";
+import Checkbox from "@material-ui/core/Checkbox";
 import "./style.css";
 import RandoAnim from "../../components/RandoAnim";
 import axios from "axios";
@@ -18,9 +19,14 @@ const useStyles = makeStyles(theme => ({
 
 export default function Eats() {
 	const classes = useStyles();
+	const [checkBoxOne, setCheckBoxOne] = React.useState(false);
+	const [checkBoxTwo, setCheckBoxTwo] = React.useState(false);
+	const [checkBoxThree, setCheckBoxThree] = React.useState(false);
+	const [checkBoxFour, setCheckBoxFour] = React.useState(false);
 	// const [showDetails, setShowDetails] = useState(false);
 	const [coords, setCoords] = useState();
 	const [restaurant, setRestaurant] = useState({});
+	const [priceOptions, setPriceOptions] = useState([]);
 
 	//yelp get route basing on lat and long
 	useEffect(() => {
@@ -48,7 +54,9 @@ export default function Eats() {
 
 		if (coords) {
 			axios
-				.get(`/api/restaurants/${coords.latitude}/${coords.longitude}`)
+				.get(
+					`/api/restaurants/${coords.latitude}/${coords.longitude}?price=${priceOptions}`
+				)
 				// .then(res => res.json())
 				.then(result => {
 					// console.log(result);
@@ -90,6 +98,63 @@ export default function Eats() {
 		</div>
 	);
 
+	const handleChange = expense => {
+		switch (expense) {
+			case "$":
+				setCheckBoxOne(!checkBoxOne);
+
+				break;
+			case "$$":
+				setCheckBoxTwo(!checkBoxTwo);
+
+				break;
+			case "$$$":
+				setCheckBoxThree(!checkBoxThree);
+
+				break;
+			case "$$$$":
+				setCheckBoxFour(!checkBoxFour);
+
+				break;
+		}
+	};
+
+	useEffect(() => {
+		if (checkBoxOne) {
+			setPriceOptions([...priceOptions, "1"]);
+		} else {
+			const filteredPrices = priceOptions.filter(option => option !== "1");
+			setPriceOptions(filteredPrices);
+		}
+	}, [checkBoxOne]);
+
+	useEffect(() => {
+		if (checkBoxTwo) {
+			setPriceOptions([...priceOptions, "2"]);
+		} else {
+			const filteredPrices = priceOptions.filter(option => option !== "2");
+			setPriceOptions(filteredPrices);
+		}
+	}, [checkBoxTwo]);
+
+	useEffect(() => {
+		if (checkBoxThree) {
+			setPriceOptions([...priceOptions, "3"]);
+		} else {
+			const filteredPrices = priceOptions.filter(option => option !== "3");
+			setPriceOptions(filteredPrices);
+		}
+	}, [checkBoxThree]);
+
+	useEffect(() => {
+		if (checkBoxFour) {
+			setPriceOptions([...priceOptions, "4"]);
+		} else {
+			const filteredPrices = priceOptions.filter(option => option !== "4");
+			setPriceOptions(filteredPrices);
+		}
+	}, [checkBoxFour]);
+
 	const Rando = () => (
 		<div className="eats">
 			<div className="imgDiv">
@@ -97,10 +162,34 @@ export default function Eats() {
 			</div>
 
 			<div className="price">
-				<PriceCheckbox /> $
-				<PriceCheckbox /> $$
-				<PriceCheckbox /> $$$
-				<PriceCheckbox /> $$$$
+				<Checkbox
+					checked={checkBoxOne}
+					color="primary"
+					inputProps={{ "aria-label": "secondary checkbox" }}
+					onChange={() => handleChange("$")}
+				/>
+				$
+				<Checkbox
+					checked={checkBoxTwo}
+					color="primary"
+					inputProps={{ "aria-label": "secondary checkbox" }}
+					onChange={() => handleChange("$$")}
+				/>{" "}
+				$$
+				<Checkbox
+					checked={checkBoxThree}
+					color="primary"
+					inputProps={{ "aria-label": "secondary checkbox" }}
+					onChange={() => handleChange("$$$")}
+				/>{" "}
+				$$$
+				<Checkbox
+					checked={checkBoxFour}
+					color="primary"
+					inputProps={{ "aria-label": "secondary checkbox" }}
+					onChange={() => handleChange("$$$$")}
+				/>{" "}
+				$$$$
 			</div>
 
 			{/* rando button to show restaurant */}
