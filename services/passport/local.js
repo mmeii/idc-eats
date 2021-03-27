e1daecfe5d8f08267055b60c85466be65cd68712
Kole Gasior <jkole822@gmail.com>
2021-03-27 12:39:45 -0400

    remove serialize and deserialize user

--- services/passport/local.js
+++ services/passport/local.js
@@ -5,28 +5,20 @@ const bcrypt = require("bcrypt");
 
 passport.use(
 	new LocalStrategy(async (username, password, done) => {
-		const user = await db.User.findOne({ where: username });
+		const user = await db.User.findOne({ where: { username } });
 
 		if (!user) {
 			return done(null, false);
 		}
 
-		const hash = user[0].dataValues.password;
+		const hash = user.dataValues.password;
 
 		bcrypt.compare(password, hash, function (err, result) {
-			if (!result || !err) return done(null, false);
+			if (!result || err) {
+				return done(null, false);
+			} else {
+				done(null, user);
+			}
 		});
-
-		done(null, user);
 	})
 );
-
-passport.serializeUser((user, done) => {
-	console.log(user);
-	done(null, user[0].dataValues.id);
-});
-
-passport.deserializeUser((id, done) => {
-	console.log(id);
-	db.User.findByPk(id).then(user => done(null, user));
-});
