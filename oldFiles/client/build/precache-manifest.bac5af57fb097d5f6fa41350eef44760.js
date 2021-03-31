c52f48927b3b88a55e863406a60ffa2d471b7553
Kole Gasior <jkole822@gmail.com>
2021-03-30 20:06:51 -0400

    move client to oldFiles

---
+++ oldFiles/client/build/precache-manifest.bac5af57fb097d5f6fa41350eef44760.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91daa97dd0e31cb449df39c511716501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76c066296c5b451656a2",
+    "url": "/static/css/main.1b440455.chunk.css"
+  },
+  {
+    "revision": "aa2ad26841103d1ac508",
+    "url": "/static/js/2.f24936a4.chunk.js"
+  },
+  {
+    "revision": "57929aad9065a71a33b72b0b1c56e76d",
+    "url": "/static/js/2.f24936a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c066296c5b451656a2",
+    "url": "/static/js/main.b44f8a42.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "f486b146eb38cb3794194d0b04994259",
+    "url": "/static/media/1.f486b146.jpg"
+  },
+  {
+    "revision": "6a4cdeecdaaba97f2e59e61c085ae23d",
+    "url": "/static/media/2.6a4cdeec.jpg"
+  },
+  {
+    "revision": "ab029fb0f28b105ba609ec1e5998562c",
+    "url": "/static/media/3.ab029fb0.jpg"
+  },
+  {
+    "revision": "0560536a18c2a74150e0c3ca13260253",
+    "url": "/static/media/4.0560536a.jpg"
+  },
+  {
+    "revision": "a76333cefb30b347b355426edb5acdd1",
+    "url": "/static/media/5.a76333ce.jpg"
+  },
+  {
+    "revision": "7b871db989cf41679a3102651b1eb40b",
+    "url": "/static/media/6.7b871db9.jpg"
+  },
+  {
+    "revision": "52731735c65885198a8c994b37560c7c",
+    "url": "/static/media/7.52731735.jpg"
+  },
+  {
+    "revision": "1e6e2017bc6a509d3617b01a8235d4fb",
+    "url": "/static/media/8.1e6e2017.jpg"
+  },
+  {
+    "revision": "ecf015ac6529402efb3a4585058b351e",
+    "url": "/static/media/logo.ecf015ac.jpg"
+  }
+]);
