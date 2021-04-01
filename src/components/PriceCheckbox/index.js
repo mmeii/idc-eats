f64f6163fa2c2003771574122f3c7b30f1e01e43
Mengmei Tu <redmmeii@gmail.com>
2021-03-31 20:26:51 -0500

    clean up, delete not needed lines

--- src/components/PriceCheckbox/index.js
+++ src/components/PriceCheckbox/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 
 const PriceCheckbox = () => {
-    // const [checked, setChecked] = React.useState(true);
-
-    // const handleChange = (event) => {
-    //     setChecked(event.target.checked);
-    // };
-
     return (
-        
+
         <Checkbox
             color="secondary"
             inputProps={{ 'aria-label': 'secondary checkbox' }}
