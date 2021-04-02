a6ed108c3571ae6dd752ec5397c263bce4367b7f
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-04-01 21:36:06 -0400

    import reduced motion, fixed responsiveness of motion component

--- src/components/RandoAnim/index.js
+++ src/components/RandoAnim/index.js
@@ -1,24 +1,26 @@
-import  React, { useEffect } from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
+import { useReducedMotion } from "framer-motion";
 import "./style.css";
-import  BackgroundSource from "./BackgroundSource";
 
+//adding reduced motion for accessibilty
 
-    
-
-
-function RandoAnim () {
-
-
-
-    return (
-        <motion.div
-        className = "rando"
-        animate=  {{ rotate: 360}}
-        transition= {{ duration: 3, loop: Infinity, ease: "linear"}}
-        />
-    )
-  }
-
-  export default RandoAnim;
+//let animate;
+  //const shouldReduceMotion = useReducedMotion();
+  //if (isOpen) {
+    //animate = shouldReduceMotion ? { opacity: 1 } : { x: 0 }
+  //} else {
+    //animate = { rotate: 360 };
+  //};
+function RandoAnim() {
   
+  return (
+    <motion.div
+      className="rando"
+      animate={{ rotate: 360 }}
+      transition={{ duration: 3, loop: Infinity, ease: "linear" }}
+    />
+  );
+}
+
+export default RandoAnim;
